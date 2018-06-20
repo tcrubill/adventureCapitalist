113ca38fa5f40983ab20a12c9ee84f6d2c9f8923
tcrubill <40420664+tcrubill@users.noreply.github.com>
2018-06-20 14:50:23 +0200

    premier update
    premier update

---
+++ src/main/java/com/isis/adventureISIServer/capitalist/CapitalistApplication.java
@@ -0,0 +1,12 @@
+package com.isis.adventureISIServer.capitalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitalistApplication.class, args);
+	}
+}
---
+++ src/test/java/com/isis/adventureISIServer/capitalist/CapitalistApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isis.adventureISIServer.capitalist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CapitalistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
